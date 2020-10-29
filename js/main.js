0380234b7bd964997db517e2042569d57b73456d
Альбина Архипова <alba91@inbox.ru>
2020-10-29 16:47:21 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-var fireballSize = 22;
-var wizardSpeed = 3;
-var wizardWidth = 70;
+'use strict';
+const fireballSize = 22;
+const wizardSpeed = 3;
+const wizardWidth = 70;
 
 
-var getFireballSpeed = function (isMovingLeft) {
-    return isMovingLeft ? 2 : 5;
-}
+const getFireballSpeed = function (isMovingLeft) {
+  return isMovingLeft ? 2 : 5;
+};
 
-var getWizardHeight = function () {
-   return 1.337 * wizardWidth;
-}
+const getWizardHeight = function () {
+  return 1.337 * wizardWidth;
+};
 
-var getWizardX = function (fieldWidth) {
-    return (fieldWidth - wizardWidth) / 2;
-}
+const getWizardX = function (fieldWidth) {
+  return (fieldWidth - wizardWidth) / 2;
+};
 
-var getWizardY = function (fieldHeight) {
-  return  fieldHeight / 3;
-}
+const getWizardY = function (fieldHeight) {
+  return fieldHeight / 3;
+};
